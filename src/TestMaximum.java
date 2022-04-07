f3e396693301a025821520db59b34ae4cae8b265
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-04-07 18:05:03 +0530

    Find the Maximum among three strings

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class TestMaximum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the Three Decimal numbers");
-        Float x = in.nextFloat();
-        Float y = in.nextFloat();
-        Float z = in.nextFloat();
-
-        Float max = testMaximumNumber(x, y, z);
-        System.out.println("Maximum float number is: " + max);
+        System.out.println("Enter the Three String value");
+        String x = in.next();
+        String y = in.next();
+        String z = in.next();
+        String max = testMaximumNumber(x, y, z);
+        System.out.println("Maximum String is: " + max);
 
     }
 
@@ -23,13 +22,25 @@ public class TestMaximum {
         }
         return max;
     }
+
     private static Float testMaximumNumber(Float x, Float y, Float z) {
         Float max = x;
-        if(y.compareTo(max)>0)
-            max=y;
-        if(z.compareTo(max)>0)
-            max=z;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
         return max;
     }
 
+    private static String testMaximumNumber(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+
+    }
 }
