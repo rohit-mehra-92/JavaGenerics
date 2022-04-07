4c968301c7b0eef5ba7196a5abeecb331f89b48e
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-04-07 17:39:11 +0530

    Find the Maximum among three Float numbers

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class TestMaximum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the Three numbers");
-        Integer x = in.nextInt();
-        Integer y = in.nextInt();
-        Integer z = in.nextInt();
+        System.out.println("Enter the Three Decimal numbers");
+        Float x = in.nextFloat();
+        Float y = in.nextFloat();
+        Float z = in.nextFloat();
 
-        Integer max = testMaximumNumber(x, y, z);
-        System.out.println("maximum number is: " + max);
+        Float max = testMaximumNumber(x, y, z);
+        System.out.println("Maximum float number is: " + max);
 
     }
 
-    public static Integer testMaximumNumber(Integer x, Integer y, Integer z) {
+    private static Integer testMaximumNumber(Integer x, Integer y, Integer z) {
         Integer max = x;
         if (y.compareTo(max) > 0) {
             max = y;
@@ -23,4 +23,13 @@ public class TestMaximum {
         }
         return max;
     }
+    private static Float testMaximumNumber(Float x, Float y, Float z) {
+        Float max = x;
+        if(y.compareTo(max)>0)
+            max=y;
+        if(z.compareTo(max)>0)
+            max=z;
+        return max;
+    }
+
 }
